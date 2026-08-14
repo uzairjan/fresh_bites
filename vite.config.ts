ec3852c4fe6e6bfd2c475cf77c74195f6682b826
Uzair Khan <muhammaduzair751@gmail.com>
2026-08-14 12:55:40 +0100

    chore: update deployment config and node version
    - Set vite base path to relative to fix asset resolution.
    - Upgrade Node.js version to 22 for the deployment workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/fresh_bites/', // Add this line!',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
